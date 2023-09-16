2131c32ea38d151c938143bdb6b8a9870203ed28
Dom <57075586+dom-dang@users.noreply.github.com>
2023-09-16 18:25:23 -0400

    updated files

---
+++ hackmit/js/scripts.js
@@ -0,0 +1,31 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+var myVar;
+
+function loading() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
